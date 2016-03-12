b49459b756e68279276c9a287deb8e72fa66dcfc
ryanburgess <ryanb@netflix.com>
2016-03-12 13:47:02 -0800

    update build file

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 var fs = require('fs');
-var content = '';
+var content = '# 2016 Web Development Conferences\nA list of 2016 web development conferences.';
 
-fs.readFile('./list.json', 'utf8', function (err, data) {
-  if (err) throw err;
-  var obj = JSON.parse(data);
-
-  for (var key in obj) {
-    content += '\n## [' + obj[key].title + '](' + obj[key].url + ')\n';
-    content += '**Where:** ' + obj[key].where + '\n\n';
-    content += '**When:** ' + obj[key].when + '\n\n';
-  }
-
-  fs.appendFile('./README.md', content, function (err) {
+function buildReadMe(){
+  fs.readFile('./list.json', 'utf8', function (err, data) {
     if (err) throw err;
+    var obj = JSON.parse(data);
+
+    for (var key in obj) {
+      content += '\n## [' + obj[key].title + '](' + obj[key].url + ')\n';
+      content += '**Where:** ' + obj[key].where + '\n\n';
+      content += '**When:** ' + obj[key].when + '\n\n';
+    }
+
+    fs.write('./README.md', content, function (err) {
+      if (err) throw err;
+    });
   });
-});
+}
