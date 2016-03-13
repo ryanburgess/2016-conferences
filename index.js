affa0c8c628f29cadc507410e8d4db47ba00b29f
ryanburgess <ryanb@netflix.com>
2016-03-12 17:12:28 -0800

    use rxjs

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var fs = require('fs');
-var obj = require('./list.json');
+const fs = require('fs');
+const obj = require('./list.json');
+const Rx = require('rx');
 var content = '# 2016 Web Development Conferences\nA list of 2016 web development conferences.\n';
 
-obj.forEach(function(conference) {
+const Observable = Rx.Observable;
+Observable.from(obj).subscribe(conference => {
   content += '\n## [' + conference.title + '](' + conference.url + ')\n';
   content += '**Where:** ' + conference.where + '\n\n';
   content += '**When:** ' + conference.when + '\n\n';
@@ -10,5 +12,5 @@ obj.forEach(function(conference) {
 
 fs.writeFile('./README.md', content, function (err) {
   if (err) throw err;
+  console.log('Updated conference list');
 });
-
