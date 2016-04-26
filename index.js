446c4e02c8b09fd57e85e44f5220a659ab7880a4
ryanburgess <ryanb@netflix.com>
2016-04-26 08:24:08 -0700

    remove RxJS as a dependency

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 'use strict';
 const fs = require('fs');
 const obj = require('./list.json');
-const Rx = require('rx');
 let content = '# 2016 Web Development Conferences\nA list of 2016 web development conferences.\n\n';
 
+// create contributing instructions
 const contribute = ('## Contributing \n' +
 '1. Fork it\n' +
-'2. Run `npm install`\n' +
-'3. Add your conference to `list.json`\n' +
-'4. Run `node index` to update `README.md` with your changes\n' +
-'5. Create your feature branch (`git checkout -b my-new-feature`)\n' +
-'6. Commit your changes (`git commit -am "Add some feature"`)\n' +
-'7. Push to the branch (`git push origin my-new-feature`)\n' +
-'8. Create new Pull Request');
+'2. Add your conference to `list.json`\n' +
+'3. Run `node index` to update `README.md` with your changes\n' +
+'4. Create your feature branch (`git checkout -b my-new-feature`)\n' +
+'5. Commit your changes (`git commit -am "Add some feature"`)\n' +
+'6. Push to the branch (`git push origin my-new-feature`)\n' +
+'7. Create new Pull Request');
 
 content += contribute + '\n';
 
+// create heading for conference list
 content += '\n#Conference List\n';
 
-const Observable = Rx.Observable;
-Observable.from(obj).subscribe(conference => {
+// create list of conferences
+for (const conference of obj) {
   content += (
     `\n## [${conference.title}](${conference.url})
 **Where:** ${conference.where}\n
 **When:** ${conference.when}\n\n`
   );
-});
+}
 
+// create README with the list of conferences
 fs.writeFile('./README.md', content, function (err) {
   if (err) throw err;
   console.log('Updated conference list');
